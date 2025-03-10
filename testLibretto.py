ce5dcdf2434c3a4787ae07b3fe36c01e741b6710
lpass <s310842@studenti.polito.it>
2025-03-10 11:11:09 +0100

    fine esercizio libretto

--- testLibretto.py
+++ testLibretto.py
@@ -25,3 +25,43 @@ if votoTrasfigurazione is None:
     print("Voto non trovato")
 else:
     print(votoTrasfigurazione.materia)
+
+print(mylib.hasVoto(v1))
+print(mylib.hasVoto(Voto("Magia", 30, "2023-07-10", False)))
+print(mylib.hasVoto(Voto("Difesa contro le arti oscure", 25, "2022-01-30", False)))
+
+print(mylib.hasConflitto(Voto("Pozioni", 27, "2022-02-12", False)))
+
+print("Test append modificata")
+mylib.append(Voto("Magia", 30, "2023-07-10", False))
+#mylib.append(Voto("Pozioni", 27, "2022-02-12", False))
+
+
+mylib.append(Voto("Divinazione", 27, "2021-02-08", False))
+mylib.append(Voto("Cura delle creature magiche", 26, "2021-06-14", False))
+
+print("----------------------------------------")
+
+
+print("----------------------------------------")
+nuovoLib = mylib.creaMigliorato()
+print("Libretto migliorato")
+print(nuovoLib)
+print("----------------------------------------")
+print("Libretto originario")
+print(mylib)
+
+print("----------------------------------------")
+print("----------------------------------------")
+ordinato = mylib.creaLibOrdinatoPerMateria()
+print("Libretto ordinato per materia")
+print(ordinato)
+print("----------------------------------------")
+ordinato2 = mylib.creaLibOrdinatoPerVoto()
+print("Libretto ordinato per materia")
+print(ordinato2)
+
+print("-----------------------------------------")
+print("Libretto a cui ho eliminato i voti inferiori a 24")
+ordinato2.cancellaInferiori(24)
+print(ordinato2)
--- voto/voto.py
+++ voto/voto.py
@@ -1,4 +1,5 @@
 import math
+import operator
 from dataclasses import dataclass
 import flet
 cfuTot = 180
@@ -15,13 +16,25 @@ class Voto:
         else:
             return f"In {self.materia} hai preso {self.punteggio} il {self.data}"
 
+    def copy(self):
+        return Voto(self.materia, self.punteggio, self.data, self.lode)
+
+    #def __eq__(self, other):
+        #return (self.materia == other.materia and
+                #self.punteggio == other.punteggio and
+                #self.lode == other.lode)
+
 class Libretto:
     def __init__(self, proprietario, voti = []):
         self.proprietario = proprietario
         self.voti = voti
 
-    def append(self, voto): # duck!
-        self.voti.append(voto)
+    def append(self, voto):
+        if (self.hasConflitto(voto) is False
+            and self.hasVoto(voto) is False):
+            self.voti.append(voto)
+        else:
+            raise ValueError("Il voto è già presente")
 
     def __str__(self):
         mystr = f"Libretto voti di {self.proprietario} \n"
@@ -73,6 +86,118 @@ class Libretto:
                 return v
 
 
+    def hasVoto(self, voto):
+        '''
+
+        verifica se il libretto contiene già il voto; due voti sono considerati uguali se hanno
+        lo stesso campo materia e lo stesso punteggio. Punteggio è formato da punteggio e lode
+        :param voto: istanza dell'oggetto voto
+        :return: True se il voto è gia presente, False altrimenti
+
+        '''
+
+        for v in self.voti:
+            # modo numero 1
+            #if v==voto:
+             #   pass
+            if (v.materia == voto.materia and v.lode == voto.lode
+                and v.punteggio == voto.punteggio):
+                return True
+        return False
+
+    def hasConflitto(self, voto):
+        '''
+        controlla che il voto non rappresenti un conflitto con i voti già presenti nel libretto
+        consideriamo conflitto quando due voti hanno lo stesso campo materia ma diverso voto
+        :param voto: istanza della classe voto'
+        :return: True se voto è in conflitto, False altrimenti
+        '''
+        for v in self.voti:
+            if (v.materia == voto.materia and not
+            (v.punteggio == voto.punteggio and v.lode == voto.lode)):
+                return True
+        return False
+
+    def copy(self):
+        '''
+        crea una nuova copia del libretto e restituisce un'istanza della classe libretto
+        :return:
+        '''
+        nuovo = Libretto(self.proprietario.copy(), [])
+        for v in self.voti:
+            nuovo.append(v.copy())
+        return nuovo
+
+    def creaMigliorato(self):
+        '''
+        crea un nuovo oggetto libretto in cui i voti sono migliorati secondo la seguente
+        logica:
+        se il voto è compreso tra 18 e 24 aggiungo 1, se il voto è compreso tra 24 e 28 aggiungo 2,
+        se il voto è 29 aggiungo 1, se è 30 rimane 30
+        :return: nuovo Libretto
+        '''
+        nuovo = self.copy()
+
+        for v in nuovo.voti:
+            if 18 <= v.punteggio < 24:
+                v.punteggio += 1
+            elif 24 <= v.punteggio <29:
+                v.punteggio += 2
+            elif v.punteggio == 29:
+                v.punteggio = 30
+        return nuovo
+
+    def sortByMateria(self):
+        #self.voti.sort(key=estraiMateria)
+        self.voti.sort(key=operator.attrgetter('materia'))
+    # ozione 1: creo due metodi stampa che ordinano e poi stampano
+    # opzione 2: creo due metodi che ordinano la lista di self e poi un unico metodo di stampa
+    # opzione 3:
+
+    def creaLibOrdinatoPerMateria(self):
+        '''
+        crea un nuovo oggetto libretto e lo ordina per materia
+        :return: nuova istanza dell'oggetto Libretto
+        '''
+        nuovo = self.copy()
+        nuovo.sortByMateria()
+        return nuovo
+
+    def creaLibOrdinatoPerVoto(self):
+        '''
+        crea un nuovo oggetto libretto e lo ordina per materia
+        :return: nuova istanza dell'oggetto Libretto
+        '''
+        nuovo = self.copy()
+        nuovo.voti.sort(key=lambda v: (v.punteggio, v.lode), reverse=True)
+        return nuovo
+
+    def cancellaInferiori(self, punteggio):
+        #modo 1
+        #for i in range(len(self.voti)):
+            #if self.voti[i].punteggio < punteggio:
+                #self.voti.pop(i)
+
+        #modo 2
+        #for v in self.voti:
+            #if v.punteggio < punteggio:
+                #self.voti.remove(v)
+        #modo 3
+        #nuovo = []
+        #for v in self.voti:
+            #if v.punteggio >= punteggio:
+                #nuovo.append(v)
+        #self.voti = nuovo
+
+        return [v for v in self.voti if v.punteggio >= punteggio]
+
+def estraiMateria(voto):
+    '''
+    questo metodo restituisce il campo materia dello stesso voto
+    :param voto: istanza della classe voto
+    :return: stringa rappresentante il nome della materia
+    '''
+    return voto.materia
 
 def testVoto():
     print("Ho usato Voto in maniera standalone")
